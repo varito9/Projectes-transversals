c052ff70681ffc5cc822c0f143f86985b7fbccf1
Alvaro <alvarosaldanaaa@gmail.com>
2025-09-16 12:08:48 +0200

    .

--- js/data.js
+++ js/data.js
@@ -4,62 +4,62 @@ export default {
       "pregunta": "De quin país és aquesta bandera?",
       "resposta_correcta": "Japó",
       "respostes_incorrectes": ["Xina", "Corea del Sud", "Bangla Desh"],
-      "imatge": "https://example.com/flags/japo.png"
+      "imatge": "img/japon.png"
     },
     {
       "pregunta": "Quin país té com a capital Canberra?",
       "resposta_correcta": "Austràlia",
       "respostes_incorrectes": ["Nova Zelanda", "Fiji", "Estats Units"],
-      "imatge": "https://example.com/flags/australia.png"
+      "imatge": "/img/australia.png"
     },
     {
       "pregunta": "De quin país és aquesta bandera?",
       "resposta_correcta": "Noruega",
       "respostes_incorrectes": ["Islàndia", "Suècia", "Finlàndia"],
-      "imatge": "https://example.com/flags/noruega.png"
+      "imatge": "/img/norway.png"
     },
     {
       "pregunta": "De quin país és aquesta bandera?",
       "resposta_correcta": "Índia",
-      "respostes_incorrectes": ["Bangla Desh", "Sri Lanka", "Pakistan"],
-      "imatge": "https://example.com/flags/india.png"
+      "respostes_incorrectes": ["BanglaDesh", "Sri Lanka", "Pakistan"],
+      "imatge": "/img/india.png"
     },
     {
       "pregunta": "De quin país és aquesta bandera?",
       "resposta_correcta": "Canadà",
       "respostes_incorrectes": ["Dinamarca", "Suïssa", "Noruega"],
-      "imatge": "https://example.com/flags/canada.png"
+      "imatge": "/img/canada.png"
     },
     {
       "pregunta": "De quin país és aquesta bandera?",
       "resposta_correcta": "Sud-àfrica",
       "respostes_incorrectes": ["Zàmbia", "Namíbia", "Botswana"],
-      "imatge": "https://example.com/flags/sud_africa.png"
+      "imatge": "/img/sudafrica.png"
     },
     {
       "pregunta": "De quin país és aquesta bandera?",
       "resposta_correcta": "Vietnam",
       "respostes_incorrectes": ["Xina", "Corea del Nord", "Singapur"],
-      "imatge": "https://example.com/flags/xina.png"
+      "imatge": "/img/vietnam.png"
     },
     {
       "pregunta": "De quin país és aquesta bandera?",
       "resposta_correcta": "Aràbia Saudita",
       "respostes_incorrectes": ["Jordània", "Emirats Àrabs Units", "Kuwait"],
-      "imatge": "https://example.com/flags/arabia_saudita.png"
+      "imatge": "/img/arabiasaudi.png"
     },
     {
       "pregunta": "De quin país és aquesta bandera?",
       "resposta_correcta": "Suècia",
       "respostes_incorrectes": ["Finlàndia", "Noruega", "Dinamarca"],
-      "imatge": "https://example.com/flags/suecia.png"
+      "imatge": "/img/suecia.png"
     },
 
     {
       "pregunta": "De quin país és aquesta bandera?",
       "resposta_correcta": "Turquia",
       "respostes_incorrectes": ["Tunísia", "Azerbaidjan", "Marroc"],
-      "imatge": "https://example.com/flags/turquia.png"
+      "imatge": "/img/turquia.png"
     }
   ]
 }
--- js/script.js
+++ js/script.js
@@ -18,13 +18,13 @@ let htmlString = "";
 
 for (let i = 0; i < data.preguntes.length; i++) {
     htmlString += `<h3>${data.preguntes[i].pregunta}</h3>`;
-    htmlString += `<img src="${data.preguntes[i].imatge}" alt="imatge pregunta ${i+1}"><br>`;
+    htmlString += `<img class='bandera' src="${data.preguntes[i].imatge}" alt="imatge pregunta ${i+1}"><br>`;
 
     
-    // Combinar respuesta correcta e incorrectas
+    //operador spread combina arrays y strings 
     let respostes = [data.preguntes[i].resposta_correcta, ...data.preguntes[i].respostes_incorrectes];
     
-    // Mezclar aleatoriamente
+    // Mezclar todas las preguntas para que no esten siempre en la misma posición
     respostes.sort(() => Math.random() - 0.5);
 
     for (let j = 0; j < respostes.length; j++) {
